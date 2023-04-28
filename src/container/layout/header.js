583069e93b89e8634033383f0ddac1cc4e3c933f
matinsangabi <sangabiwork@gmail.com>
2023-04-28 16:44:03 +0430

    first section added

--- src/container/layout/header.js
+++ src/container/layout/header.js
@@ -8,7 +8,7 @@ const navigation = [
 ];
 const Header = () => {
   return (
-    <header className="sticky top-0 w-full transition-all ease-in-out duration-100 z-20 backdrop-blur-2xl blur-0 opacity-100">
+    <header className="sticky top-0 w-full transition-all ease-in-out duration-100 z-40 backdrop-blur-2xl blur-0 opacity-100">
       <div className="container xl:max-w-screen-xl top-0 py-2 md:px-0 mx-auto">
         <nav className="flex items-center justify-between">
           <Link href={"/"}>
@@ -21,7 +21,6 @@ const Header = () => {
           </Link>
           <div className="flex items-center gap-x-20">
             <ul className="flex items-center gap-x-10">
-
               {navigation.map((nav, i) => {
                 return (
                   <Link
@@ -34,7 +33,9 @@ const Header = () => {
                 );
               })}
             </ul>
-            <button className="py-4 px-8  rounded-full  text-white bg-blue-950 text-xs tracking-wider">مرکز تماس</button>
+            <button className="py-4 px-8  rounded-full  text-white bg-blue-950 text-xs tracking-wider">
+              مرکز تماس
+            </button>
           </div>
         </nav>
       </div>
--- src/container/layout/index.js
+++ src/container/layout/index.js
@@ -2,7 +2,7 @@ import Header from "./header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="h-full min-h-screen bg-gradient-to-bl from-blue-100 from-20% via-blue-100 via-20% to-slate-100 to-90%">
+    <div className="h-full min-h-screen bg-gradient-to-bl from-blue-200 from-5% via-blue-100 via-30% to-slate-100 to-90%">
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-1">{children}</main>
