d561c7fb36a8bdf3608e2b690925bc4aeaa936a7
matinsangabi <sangabiwork@gmail.com>
2023-04-29 20:34:58 +0430

    responsive header fixed

--- src/container/layout/header.js
+++ src/container/layout/header.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import { HiBars3BottomRight } from "react-icons/hi2";
 const navigation = [
   { name: "خانه", path: "/" },
   { name: "درباره ما", path: "/about" },
@@ -8,9 +8,21 @@ const navigation = [
 ];
 const Header = () => {
   return (
-    <header className="sticky top-0 w-full transition-all ease-in-out duration-100 z-40 backdrop-blur-2xl blur-0 opacity-100">
-      <div className="container xl:max-w-screen-xl top-0 py-2 md:px-0 mx-auto">
+    <header className="sticky top-0 w-full transition-all ease-in-out duration-100 z-40 backdrop-blur-xl blur-0 opacity-100">
+      <div className="container xl:max-w-screen-xl top-0 py-2  mx-auto px-2 md:px-0">
         <nav className="flex items-center justify-between">
+          <div className="w-full md:hidden flex items-center justify-between">
+            <Link href={"/"}>
+              <img
+                src={"/images/mobilelogo.png"}
+                width={35}
+                className="block md:hidden"
+              />
+            </Link>
+            <button className="text-lg text-blue-950">
+              <HiBars3BottomRight />
+            </button>
+          </div>
           <Link href={"/"}>
             <img
               src={"/images/logo.png"}
@@ -19,7 +31,7 @@ const Header = () => {
               className="hidden md:block"
             />
           </Link>
-          <div className="flex items-center gap-x-20">
+          <div className="hidden md:flex items-center gap-x-20">
             <ul className="flex items-center gap-x-10">
               {navigation.map((nav, i) => {
                 return (
